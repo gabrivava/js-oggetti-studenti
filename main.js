3ae75bd41783631b0895924a0896f18785d9861e
gabrivava <gabriele.vavassori4@gmail.com>
2021-05-07 15:52:52 +0200

    errore classificazione studenti

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 /* 
 
-Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
 
 //Creare un oggetto che descriva uno studente, con le seguenti proprietà:
 /* nome,
 cognome
 età. */
-var studente = {
+var studenteIo = {
     nome: 'gabriele',
     cognome: 'vavassori',
     età: 23
 }
-console.log(studente);
 
 //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 var datiStudenteEl = document.getElementById('dati_studente');
-for (var key in studente) {
-    console.log(key, studente[key]);
-    datiStudenteEl.insertAdjacentHTML('afterend', 
+for (var key in studenteIo) {
+    datiStudenteEl.insertAdjacentHTML('beforeend', 
     `
-    <p> ${key}: ${studente[key]} </p>
+    <p> ${key}: ${studenteIo[key]} </p>
     `
     )
 }
@@ -31,6 +28,29 @@ var classe = [
         nome: 'franco',
         cognome: 'bisio',
         età: 35
+    },
+    {
+        nome: 'luigi',
+        cognome: 'rossi',
+        età: 55
     }
+];
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+var datiClasseEl = document.getElementById('dati_classe');
+for (var i = 0; i < classe.length; i++) {
+    var studente = classe[i];
+    datiClasseEl.innerHTML =
+    `
+    <h4> Studente ${i}: </h4>
+    `
     
-]
+    for (var key in studente) {
+        console.log(key, studente[key]);
+        datiClasseEl.insertAdjacentHTML('beforeend', 
+        `
+        <p> ${key}: ${studente[key]} </p>
+        `
+        )
+    }
+}
